1a17baae81a581f5855ff2d28f71a514e338c1a9
just-me- <hess-marcel>
2014-04-14 16:21:42 +0200

    Regex Abschluss vorerst

--- kapitel5/5-regex.pl
+++ kapitel5/5-regex.pl
@@ -1,22 +1,24 @@
 #!/usr/bin/perl -w
 
 # Mindestens ein a gefolgt von beliebig vielen b
-# m/a*b*/g  #m kann - und wird auch meist - weggelassen 
+# m/a+b*/g  #m kann - und wird auch meist - weggelassen 
 #
 # Eine beliebige Anzahl von Backslashes (\) gefolgt von einer beliebigen anzahl von Sternen
 # m/\\*\**/g
 # 
 # Drei aufeianderfolgende Kopien des Strings der in $what enthalten ist
-# m/$what/o
+# m/($what){3}/o
 
 $count = 0; #Anzahl Vokale
+$words = 0; #Anzahl Woerte mit 2x Doppelbuchstaben
 
 open(WORDS, "<words.txt") || die "Datei nicht gefunden\n";
 while (<WORDS>){
 	$count++ if (/a/i and /e/i and /i/i and /o/i and /u/i); 
+	$words++ if (/(.)\1.*(.)$2.*/);
 }
 close(WORDS);
 
-print "Anzahl: $count";
+print "Anzahl mit Vokalen: $count\n Anzahl Woerter mit 2x doppelt: $words";
 
 
