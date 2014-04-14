9c084d664d77844730511137a2dc5cbcc200e5c5
root <root@KR56-01.(none)>
2014-04-14 15:42:22 +0200

    Regex Einleitung

---
+++ kapitel5/5-regexp.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+# Mindestens ein a gefolgt von beliebig vielen b
+# m/a*b*/g  #m kann - und wird auch meist - weggelassen 
+#
+# Eine beliebige Anzahl von Backslashes (\) gefolgt von einer beliebigen anzahl von Sternen
+# m/\\*\**/g
+# 
+# Drei aufeianderfolgende Kopien des Strings der in $what enthalten ist
+# m/$what/o
+#
+#
+#
+#
